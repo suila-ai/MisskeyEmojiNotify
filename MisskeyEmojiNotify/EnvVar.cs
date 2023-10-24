7508c8279fd412ebe58b5ba459b7f6e37d95fc9f
suila-ai <suila_ai@fuji-discretize.dev>
2023-10-24 22:57:48 +0900

    change: 環境変数取得方法

--- MisskeyEmojiNotify/EnvVar.cs
+++ MisskeyEmojiNotify/EnvVar.cs
@@ -8,12 +8,27 @@ namespace MisskeyEmojiNotify
 {
     internal class EnvVar
     {
-        public static string? MisskeyServer { get; } = Environment.GetEnvironmentVariable("MISSKEY_SERVER");
-        public static string? MisskeyToken { get; } = Environment.GetEnvironmentVariable("MISSKEY_TOKEN");
+        public static string MisskeyServer { get; } = GetEnvVar("MISSKEY_SERVER");
+        public static string MisskeyToken { get; } = GetEnvVar("MISSKEY_TOKEN");
 
-        public static TimeSpan CheckInterval { get; } = TimeSpan.FromSeconds(int.Parse(Environment.GetEnvironmentVariable("MISSKEY_CHECK_INTERVAL") ?? "600"));
-        public static string Visibility { get; } = Environment.GetEnvironmentVariable("MISSKEY_VISIBILITY") ?? "specified";
+        public static TimeSpan CheckInterval { get; } = TimeSpan.FromSeconds(int.Parse(GetEnvVar("MISSKEY_CHECK_INTERVAL", "600")));
+        public static string Visibility { get; } = GetEnvVar("MISSKEY_VISIBILITY", "specified");
 
-        public static string ArchiveFile { get; } = Environment.GetEnvironmentVariable("MISSKEY_ARCHIVE_FILE") ?? "./archive.json";
+        public static string ArchiveFile { get; } = GetEnvVar("MISSKEY_ARCHIVE_FILE", "./archive.json");
+
+        private static string GetEnvVar(string name, string? defaultValue = null)
+        {
+            var envVar = Environment.GetEnvironmentVariable(name);
+            if (envVar != null) return envVar;
+            if (defaultValue != null) return defaultValue;
+            throw new MissingEnvVarException($"\"{name}\" is required");
+        }
+
+        internal class MissingEnvVarException : Exception
+        {
+            public MissingEnvVarException(string? message) : base(message)
+            {
+            }
+        }
     }
 }
