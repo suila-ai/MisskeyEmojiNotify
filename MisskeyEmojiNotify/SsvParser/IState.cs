3538e6271bf229b5d529a74ccce06575b7349c52
suila-ai <suila_ai@fuji-discretize.dev>
2023-12-05 15:24:10 +0900

    add: ssv parser

---
+++ MisskeyEmojiNotify/SsvParser/IState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisskeyEmojiNotify.SsvParser
+{
+    internal interface IState
+    {
+        public int Index { get; }
+
+        public IState Next(char c);
+    }
+
+    internal interface IFieldState : IState
+    {
+        public int Start { get; }
+        public int End { get; }
+    }
+}
---
+++ MisskeyEmojiNotify/SsvParser/Ssv.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisskeyEmojiNotify.SsvParser
+{
+    internal static class Ssv
+    {
+        public static IEnumerable<string> Parse(string text)
+        {
+            IState state = new SeparatedState(-1);
+
+            foreach (var c in text.Append('\0'))
+            {
+                var nextState = state.Next(c);
+
+                if (state is IFieldState fieldState && nextState is SeparatedState)
+                {
+                    yield return text[fieldState.Start..(fieldState.End + 1)];
+                }
+
+                state = nextState;
+            }
+        }
+    }
+}
